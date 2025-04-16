2eaaa04b164a3817bfb339c67dce058a13a3d21d
Hi, I am Priya! <priyajindal48011@gmail.com>
2025-04-16 09:59:39 -0700

    Update WN10-CC-000065.ps1

--- STIGS/WN10-CC-000065.ps1
+++ STIGS/WN10-CC-000065.ps1
@@ -1,3 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the Wi-Fi Sense is disabled.
+
+.NOTES
+    Author          : Priya
+    LinkedIn        : https://www.linkedin.com/in/priya-priya-a36a94222/ 
+    GitHub          : https://github.com/priya48011/cybersecurity
+    Date Created    : 2025-04-16
+    Last Modified   : 2025-04-16
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000065
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+  
+    PS C:\> .\STIG-ID-WN10-CC-000065
+#>
 
 # Define the registry path and values
 $registryPath = "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config"
