440840a87a8ad37114ea26e11077a75de3d8632b
Hi, I am Priya! <priyajindal48011@gmail.com>
2025-11-18 07:02:29 -0800

    Update WN10-AU-000005.ps1

--- STIGS/WN10-AU-000005.ps1
+++ STIGS/WN10-AU-000005.ps1
@@ -1,1 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that system is configured to audit Account Logon - Credential Validation failures.
+.NOTES
+    Author          : Priya
+    LinkedIn        : https://www.linkedin.com/in/priya-priya-a36a94222/ 
+    GitHub          : https://github.com/priya48011/cybersecurity
+    Date Created    : 2025-11-17
+    Last Modified   : 2025-11-17
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000005
 
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+  
+    PS C:\> .\STIG-ID-WN10-AU-000005
+#>
+
+
+# Enable Failure auditing for Credential Validation
+auditpol /set /subcategory:"Credential Validation" /failure:enable
+
+Write-Host "WN10-AU-000005 remediated: 'Credential Validation' now audits Failure events."
