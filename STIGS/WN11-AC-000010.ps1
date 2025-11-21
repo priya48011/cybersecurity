668ef03688ee4a5b753e988e53871835a668bc61
Hi, I am Priya! <priyajindal48011@gmail.com>
2025-11-21 11:18:22 -0800

    Update WN11-AC-000010.ps1

--- STIGS/WN11-AC-000010.ps1
+++ STIGS/WN11-AC-000010.ps1
@@ -1,1 +1,39 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the number of allowed bad logon attempts is configured to three or less. 
+.NOTES
+    Author          : Priya
+    LinkedIn        : https://www.linkedin.com/in/priya-priya-a36a94222/ 
+    GitHub          : https://github.com/priya48011/cybersecurity
+    Date Created    : 2025-11-21
+    Last Modified   : 2025-11-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AC-000010
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+  
+    PS C:\> .\STIG-ID-WN11-AC-000010
+#>
+
+# Export current local security policy
+secedit.exe /export /cfg C:\Windows\Temp\secpol.cfg
+
+# Set Account Lockout Threshold = 3 attempts
+(Get-Content C:\Windows\Temp\secpol.cfg) `
+    -replace 'LockoutBadCount = \d+', 'LockoutBadCount = 3' |
+    Set-Content C:\Windows\Temp\secpol.cfg
+
+# Apply updated policy
+secedit.exe /configure /db C:\Windows\security\local.sdb /cfg C:\Windows\Temp\secpol.cfg /areas SECURITYPOLICY
+
+# Cleanup
+Remove-Item C:\Windows\Temp\secpol.cfg -Force
 
