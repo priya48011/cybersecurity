b1c1e8c5ba84828c6aba1d311142b3319cd57624
Hi, I am Priya! <priyajindal48011@gmail.com>
2025-11-21 11:24:11 -0800

    Update WN11-CC-000315.ps1

--- STIGS/WN11-CC-000315.ps1
+++ STIGS/WN11-CC-000315.ps1
@@ -1,1 +1,29 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the Windows Installer feature "Always install with elevated privileges" is disabled
+.NOTES
+    Author          : Priya
+    LinkedIn        : https://www.linkedin.com/in/priya-priya-a36a94222/ 
+    GitHub          : https://github.com/priya48011/cybersecurity
+    Date Created    : 2025-11-21
+    Last Modified   : 2025-11-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000315
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+  
+    PS C:\> .\STIG-ID-WN11-CC-000315
+#>
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer" `
+    -Name "AlwaysInstallElevated" -Value 0 -PropertyType DWord -Force
+
 
