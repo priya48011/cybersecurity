7b5feef1e5feafe438fc302158a827ebfcf2a227
Hi, I am Priya! <priyajindal48011@gmail.com>
2025-11-21 11:12:11 -0800

    Update WN11-CC-000285.ps1

--- STIGS/WN11-CC-000285.ps1
+++ STIGS/WN11-CC-000285.ps1
@@ -1,1 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the remote desk host requires secure RPC communication
+.NOTES
+    Author          : Priya
+    LinkedIn        : https://www.linkedin.com/in/priya-priya-a36a94222/ 
+    GitHub          : https://github.com/priya48011/cybersecurity
+    Date Created    : 2025-11-21
+    Last Modified   : 2025-11-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000285
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+  
+    PS C:\> .\STIG-ID-WWN11-CC-000285
+#>
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" `
+    -Name "fEncryptRPCTraffic" -Value 1 -PropertyType DWord -Force
 
